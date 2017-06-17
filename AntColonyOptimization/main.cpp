4b393a1c75c590181eb12ba8455f0f44ba3e0ee6
Sebastian Wilde Alarcon Arenas <sebastian.alarcon@ucsp.edu.pe>
2017-06-17 00:51:10 -0500

    trabajos

--- AntColonyOptimization/main.cpp
+++ AntColonyOptimization/main.cpp
@@ -11,12 +11,14 @@ using namespace std;
 int main()
 {
     ACO * aco;
-    aco = new ACO(5,1,5,0.5,4,4,5,0.000001,100);
+    aco = new ACO(5,1,5,0.5,6,6,5,0.000001,100);
     //(int maxit, double alpha, double beta, double ro, int nAnts, int nCiudades, int b, double taumin, int q)
     aco ->cPosicion(0,10.1,20.1);
     aco ->cPosicion(1,20.1,40.1);
     aco ->cPosicion(2,30.1,10.1);
     aco ->cPosicion(3,40.1,20.1);
+    aco ->cPosicion(4,20.0,20.0);
+    aco ->cPosicion(5,30,30);
     aco ->inicializar();
 ;
 
